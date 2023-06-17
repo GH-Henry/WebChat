7ddd34c5cfe126a434b8e8e38b21073836682403
EddieIsNotAvailable <throwaway9172@protonmail.com>
2023-06-17 11:58:59 -0500

    update

--- webchat/src/main/java/uta/cse3310/WSS_App.java
+++ webchat/src/main/java/uta/cse3310/WSS_App.java
@@ -12,26 +12,41 @@ import org.java_websocket.drafts.Draft_6455;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
+import com.google.gson.Gson;
 
-public class App extends WebSocketServer
+public class WSS_App extends WebSocketServer
 {
 
     public int client_id=0;
 
-    public App(int port) throws UnknownHostException {  super(new InetSocketAddress(port));     }
-    public App(InetSocketAddress address) {     super(address);     }
-    public App(int port, Draft_6455 draft) {    super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));   }
+    public WSS_App(int port) throws UnknownHostException {  super(new InetSocketAddress(port));     }
+    public WSS_App(InetSocketAddress address) {     super(address);     }
+    public WSS_App(int port, Draft_6455 draft) {    super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));   }
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
-        conn.send("Welcome to the server! Your client id: " + client_id++); //This method sends a message to the new client
-        broadcast("new connection: " + handshake
+        conn.send("Welcome to the server! client id: " + client_id++); //This method sends a message to the new client
+        broadcast("New connection: " + handshake
             .getResourceDescriptor()); //This method sends a message to all clients connected
         System.out.println(
             conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!");
 
         //Requirement #2, print new client id to stdout
-        System.out.println("Client " + client_id + " joined"); 
+        System.out.println("Client " + (client_id-1) + " joined");
+
+
+        class Client {
+            int client_id;
+            Client(int _client_id) {    client_id = _client_id;    }
+        }
+
+        Gson gson = new Gson();
+        Client c = new Client(client_id);
+
+        conn.send(gson.toJson(c)); //Send the client id in json
+        System.out.println(gson.toJson(c));
+
+
     }
 
     @Override
@@ -62,7 +77,7 @@ public class App extends WebSocketServer
 
     @Override
     public void onStart() {
-        System.out.println("Server started!");
+        System.out.println("WebSocket server started!");
         setConnectionLostTimeout(0);
         setConnectionLostTimeout(100);
     }
@@ -72,20 +87,15 @@ public class App extends WebSocketServer
     {
         //Setup the http server
         int port = 8080;
-
-        //HttpServer H = new HttpServer(port, "./html");
-        HTTPServer h = new HTTPServer(port);
+        HttpServerImplementation h = new HttpServerImplementation(port, "./html");
         h.start();
         System.out.println("\n\nhttp Server started on port: " + port + "\n\n");
 
         //Create and start websocket server
-
         port = 8081;
-        App a = new App(port);
+        WSS_App a = new WSS_App(port);
         a.start();
-        System.out.println("\n\nwebsocket Server started on port: " + port + "\n\n");
+        System.out.println("\n\nWebSocket Server started on port: " + port + "\n\n");
     }
-
-    
 
 }
