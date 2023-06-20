e9f627bd4e8b43e48229bd78690433726954a544
Januar Soepangat <januar.soepangat@gmail.com>
2023-06-19 23:12:13 -0500

    Pull from Eddie's code

--- webchat/src/main/java/uta/cse3310/App.java
+++ webchat/src/main/java/uta/cse3310/App.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 import java.util.Collections;
 
 import org.java_websocket.WebSocket;
@@ -13,85 +14,86 @@ import org.java_websocket.drafts.Draft_6455;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
-import uta.cse3310.HTTPServer;
+import com.google.gson.Gson;
+
+// import uta.cse3310.HTTPServer;
 
 /**
  * Hello world!
  *
  */
 public class App extends WebSocketServer {
-    public App(int port) throws UnknownHostException {
-      super(new InetSocketAddress(port));
-    }
+    public int client_id=0;
 
-    public App(InetSocketAddress address) {
-      super(address);
-    }
-
-    public App(int port, Draft_6455 draft) {
-      super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));
-    }
+    public App(int port) throws UnknownHostException { super(new InetSocketAddress(port)); }
+    public App(InetSocketAddress address) { super(address); }
+    public App(int port, Draft_6455 draft) { super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft)); }
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
-			conn.send("Welcome to the server!");	// This method sends a message to the client
-			broadcast("new connection: " + handshake.getResourceDescriptor());	// This method sends a message to all clients connected
-			System.out.println(conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!");
+        System.out.println(
+            conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!");
+
+        System.out.println("Client " + client_id + " joined");
+
+        Gson gson = new Gson();
+        Client c = new Client(client_id);
+
+        conn.send(gson.toJson(c)); //Send the client id in json
+        System.out.println(gson.toJson(c).toString());
+
+        client_id++;
     }
 
-		@Override
-		public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-			broadcast(conn + " has left the room!");
-			System.out.println(conn + " has left the room!");
-		}
-
-		@Override
-		public void onMessage(WebSocket conn, String message) {
-			broadcast(message);
-			System.out.println(conn + ": " + message);
-		}
-
-    public static void main( String[] args ) throws InterruptedException, IOException {
-			int httpPort = 8080;
-			int port = 8081;
-
-			// try {
-			// 	port = Integer.parseInt(args[0]);
-			// } catch (Exception ex) {
-
-			// }
-
-			HTTPServer h = new HTTPServer(httpPort, "./html");
-			h.start();
-			System.out.println("HTTPServer started on port: " + httpPort);
-
-			App s = new App(port);
-			s.start();
-			System.out.println("ChatServer started on port: " + s.getPort());
-
-			BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
-			while (true) {
-				String in = sysin.readLine();
-				s.broadcast(in);
-				if (in.equals("exit")) {
-					s.stop(1000);
-					break;
-				}
-			}
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        broadcast(conn + " has left the room!");
+        System.out.println(conn + " has left the room!");
     }
 
-		@Override
-		public void onError(WebSocket conn, Exception ex) {
-			ex.printStackTrace();
-			if (conn != null) {
-				// some errors like port binding failed may not be assignable to a specific websocket
-			}
-		}
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        System.out.println(conn + ": " + message);
+    }
 
-		@Override
-		public void onStart() {
-			System.out.println("Server started!");
-			setConnectionLostTimeout(0);
-			setConnectionLostTimeout(100);
-		}
+    @Override
+    public void onMessage(WebSocket conn, ByteBuffer message) {
+        System.out.println(conn + ": " + message);
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        ex.printStackTrace();
+        if (conn != null) {
+        // some errors like port binding failed may not be assignable to a specific websocket
+        }
+    }
+
+    @Override
+    public void onStart() {
+        System.out.println("WebSocket server started!");
+        setConnectionLostTimeout(0);
+        setConnectionLostTimeout(100);
+    }
+
+
+    public static void main( String[] args ) throws IOException
+    {
+        //Setup the http server
+        int port = 8080;
+        HttpServer h = new HttpServer(port, "./client");
+        h.start();
+        System.out.println("\n\nhttp Server started on port: " + port + "\n\n");
+
+        //Create and start websocket server
+        port = 8081;
+        App a = new App(port);
+        a.start();
+        System.out.println("\n\nWebSocket Server started on port: " + port + "\n\n");
+    }
+
+    class Client {
+            int client_id;
+            Client(int _client_id) {    client_id = _client_id;    }
+        }
 }
