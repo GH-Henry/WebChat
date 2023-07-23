29ce5c2891c18b6da398b3f4102e19f9b0bd2543
d-perry0 <111898499+d-perry0@users.noreply.github.com>
2023-07-23 17:40:29 -0500

    feedback - task 5 fix

--- webchat/src/main/java/uta/cse3310/App.java
+++ webchat/src/main/java/uta/cse3310/App.java
@@ -1,30 +1,27 @@
 package uta.cse3310;
 
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 
 public class App {
     public static void main( String[] args ) throws UnknownHostException {
         System.out.println("\nWelcome to Webchat!\n");
-        Scanner sc = new Scanner(System.in);
-        int port = 0;       
+        int HTTPport = Integer.parseInt(args[0]);
+        int WSport = Integer.parseInt(args[1]);
+
         
         //Setup the http server
         System.out.println("Enter HTTP Server port: ");
-        port = sc.nextInt();
-        HttpServerImplementation h = new HttpServerImplementation(port, "webchat/html/index.html");
+        HttpServerImplementation h = new HttpServerImplementation(HTTPport, "webchat/html/index.html");
         h.start();
-        System.out.println("http Server started on port: " + port);
+        System.out.println("http Server started on port: " + HTTPport);
 
 
         //Create and start websocket server
         System.out.println("\nEnter WebSocket Server port: ");
-        port = sc.nextInt();
-        sc.close();
-        WS a = new WS(port);
+        WS a = new WS(WSport);
         a.start();
-        System.out.println("WebSocket Server started on port: " + port);
+        System.out.println("WebSocket Server started on port: " + WSport);
 
         //Create and write to Log.txt
         System.out.println("Message history will be saved to: Log.txt");
